932fb22a4b55e234acc738a52c4fd11b2ce67b64
esra-aydemir <h.esraaydemir@hotmail.com>
2018-05-14 23:24:59 +0300

    naive bayes implementation

--- sentiment.py
+++ sentiment.py
@@ -1,15 +1,17 @@
+#%%
 from nltk import sent_tokenize, word_tokenize
 import string
 import re
 import os
 from TurkishStemmer import TurkishStemmer
 from collections import defaultdict
+from math import log
 
 #%%
 #'negative'=-1
 #'positive'=1
 #'notr'=0
-data_path="/home/hilal/Documents/cmpe493/term-project/train/"
+data_path="train/"
 stopwords_path="stopwords.txt"
 #%%
 def fill_stopword(stopwords_path):    
@@ -20,6 +22,28 @@ def fill_stopword(stopwords_path):
         stoplist.append(token[0].strip()) 
     file.close()
     return stoplist
+
+#%% lower for turkisch characters
+def trlower(str):
+    res = ""
+    for c in str:
+        if c =='ı':
+            res=res+'ı'
+        elif c =='Ğ'or c =='ğ':
+            res=res+'ğ'
+        elif c =='Ü' or c=='ü':
+            res=res+'ü'
+        elif c =='Ş' or c =='ş':
+            res=res+'ş'
+        elif c =='İ':
+            res=res+'i'
+        elif c =='Ö' or c =='ö':
+            res=res+'ö'
+        elif c =='Ç' or c =='ç':
+            res=res+'ç'
+        else:
+            res=res + c.lower()
+    return res
 #%%
  
 def read_file(file_path,tokens,data_id,data_labels,label,stoplist,stemmer,positional_stem):
@@ -34,7 +58,7 @@ def read_file(file_path,tokens,data_id,data_labels,label,stoplist,stemmer,positi
             sentences = re.sub(r"'(\S)*(\s)",' ',sentences)    
             
             #print(sentences)
-            tokenized_sents = [word_tokenize(sent.lower())  for sent in sent_tokenize(sentences) ]        
+            tokenized_sents = [word_tokenize(trlower(sent))  for sent in sent_tokenize(sentences) ]        
             data_id.append(tokenized_sents[0][0])
             temp=[]
             for each_sentence in tokenized_sents:
@@ -94,4 +118,36 @@ stoplist=fill_stopword(stopwords_path)
 tokens,data_id,data_labels,positional_stem=read_all_file(data_path,stoplist,stemmer)     
 
 #%%
-        
+wordCountsByClass = [dict(),dict(),dict()]#neg,notr,pos  /-1 0 1
+for i in range(len(data_labels)):
+    for w in tokens[i]:
+        if w in wordCountsByClass[data_labels[i]+1]:
+            wordCountsByClass[data_labels[i]+1][w]+=1
+        else:
+            wordCountsByClass[data_labels[i]+1][w]=1
+
+#%%
+def p_snt_in_class(snt,cls):
+    lenn = sum(wordCountsByClass[cls].values())
+    vocLen = len(wordCountsByClass[cls])
+    p=log(lenn/(sum(wordCountsByClass[0].values())+sum(wordCountsByClass[1].values())+sum(wordCountsByClass[2].values())))#probability of class
+    for w in snt:
+        if w in wordCountsByClass[cls]:
+            p+=log((wordCountsByClass[cls][w])/(lenn+vocLen))
+        else:    
+            p+=log(1/(lenn+vocLen))
+    return p    
+ 
+#%%
+def m_a_p(snt):
+    mapcls = 0#default
+    p = p_snt_in_class(snt,0)
+    for i in range(len(wordCountsByClass)):
+        pnew = p_snt_in_class(snt,i)
+        if pnew>p:
+            p = pnew
+            mapcls=i
+    return mapcls-1#returns sentiment of max a posteriori class
+ 
+#%%
+      
